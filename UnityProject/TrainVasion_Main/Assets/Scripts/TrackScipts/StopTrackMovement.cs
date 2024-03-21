b299af64ba02be4587b2ad19291857843dbe895b
ShrkeyAMB <mymshrkeymym@gmail.com>
2024-03-20 19:44:40 -0700

    moreTrackWork

---
+++ UnityProject/TrainVasion_Main/Assets/Scripts/TrackScipts/StopTrackMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopTrackMovement : MonoBehaviour
+{
+    public TrackManager TM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TM = GameObject.Find("TrackManager").GetComponent<TrackManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(0, 0, 0), TM.speed * Time.deltaTime);
+
+        if (transform.position.z == -15)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
