7afd1d968e98c34bccb0b592f4a995076c5ac72c
Setseth <48038201+Setseth@users.noreply.github.com>
2024-03-20 19:20:00 -0700

    Speed

--- UnityProject/TrainVasion_Main/Assets/Scripts/PlayerMovement.cs
+++ UnityProject/TrainVasion_Main/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,12 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rbPlayer;
     private Vector3 direction = Vector3.zero;
     public float speed = 10.0f;
-    public GameObject[] spawnPoints = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
@@ -17,18 +22,9 @@ public class PlayerMovement : MonoBehaviour
         direction = new Vector3(horMove, 0, verMove);
     }
 
-    // Update is called once per frame
+    // Update is called for once per frame
     void FixedUpdate()
     {
-        rbPlayer.AddForce(direction * speed, ForceMode.Force);
-
-        if (transform.position.z > 40)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, 40);
-        }
-        else if (transform.position.z < -40)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -40);
-        }
+        rbPlayer.AddForce(direction, ForceMode.Impulse);
     }
 }
