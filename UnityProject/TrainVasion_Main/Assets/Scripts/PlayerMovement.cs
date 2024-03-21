7d18e78d49c7a1f277ca3579c57340678d0ef016
Setseth <48038201+Setseth@users.noreply.github.com>
2024-03-20 18:19:08 -0700

    commit

---
+++ UnityProject/TrainVasion_Main/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+    private Vector3 direction = Vector3.zero;
+    public float speed = 10.0f;
+    public GameObject[] spawnPoints = null;
+
+    private void Update()
+    {
+        float horMove = Input.GetAxis("Horizontal");
+        float verMove = Input.GetAxis("Vertical");
+
+        direction = new Vector3(horMove, 0, verMove);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rbPlayer.AddForce(direction * speed, ForceMode.Force);
+
+        if (transform.position.z > 40)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 40);
+        }
+        else if (transform.position.z < -40)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -40);
+        }
+    }
+}
